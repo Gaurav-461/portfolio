4d51a6e299a69b399177d56f82fb14a2ceca6fd7
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-07-16 22:07:40 +0530

    Add @vercel/analytics package and integrate Analytics component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,15 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/react"
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
-
 
 const App = () => {
   return (
     <>
       <main>
+        <Analytics />
         <Toaster />
         <Navbar />
         <Outlet />
