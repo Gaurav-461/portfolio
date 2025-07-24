7e65eda479960b3c994f5d7213bb8da9582a10b2
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-07-25 04:04:55 +0530

    Add gsap scrollSmoother for smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,31 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
 import { Toaster } from "sonner";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { useGSAP } from "@gsap/react";
 
-gsap.registerPlugin(ScrollTrigger, SplitText);
+gsap.registerPlugin(ScrollTrigger, SplitText, ScrollSmoother);
 
 const App = () => {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    });
+  });
   return (
     <>
       <main>
         <Analytics />
         <Toaster />
         <Navbar />
-        <Outlet />
-        <Footer />
+        <div id="smooth-wrapper">
+          <div id="smooth-content">
+            <Outlet />
+            <Footer />
+          </div>
+        </div>
       </main>
     </>
   );
