0e9172b3f70af8baecb447801295830c8576b0a2
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-08-01 20:18:49 +0530

    Refactor Play component: Update CircularBillBoard import path, enhance GSAP animation with easing, and implement Suspense for loading state.

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -1,24 +1,27 @@
 import React, { Suspense } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { Loading } from "../components/index";
 
-const CircularBillBoard = React.lazy(() => import("../components/showProjects/CircularBillBoard"));
+const CircularBillBoard = React.lazy(
+    () => import("../components/3dModels/CircularBillBoard"),
+);
 
 const ProjectsPage = () => {
     useGSAP(() => {
-      gsap.from("#ThreeDModel", {
-        opacity: 0,
-        // delay: 1,
-        duration: 3
-      })
-    })
+        gsap.from("#ThreeDModel", {
+            opacity: 0,
+            duration: 1,
+            ease: "power1.in",
+        });
+    },[]);
     return (
-            <div id="ThreeDModel" className="w-full h-dvh">
-                {/* <Suspense fallback={<Loading />}> */}
-                    <CircularBillBoard />
-                {/* </Suspense> */}
-            </div>
-    )
-}
+        <div id="ThreeDModel" className="h-dvh w-full">
+            <Suspense fallback={<Loading />}>
+                <CircularBillBoard />
+            </Suspense>
+        </div>
+    );
+};
 
-export default ProjectsPage
+export default ProjectsPage;
