9fac37083d540c45b4e021da80d534484f6790f6
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-08-01 20:19:21 +0530

    Enhance Loading component: Integrate GSAP for smooth loading animations, display loading progress percentage, and update layout with a new loader design.

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,29 @@
+import { useGSAP } from "@gsap/react";
+import { useProgress } from "@react-three/drei";
+import gsap from "gsap";
 
-const Loading = ({ message = "Loading..." }) => {
+const Loading = () => {
+  const { progress, total } = useProgress();
+  console.log(progress, total)
+
+  useGSAP(() => {
+    if (progress === 100) {
+      gsap.to(".loader-screen", {
+        yPercent: -100,
+        duration: 1,
+        ease: "power2.inOut",
+      });
+    }
+  }, [progress]);
+
   return (
-    <div className="flex-center z-[99999] h-screen w-full">
-      <h1 className="text-gradient p-5 text-4xl">{message}</h1>
+    <div className="loader-screen bg-black w-screen h-dvh fixed top-0 left-0 z-[10000000] text-amber-50">
+      <div className="flex-center w-full h-full">
+        <img src="/loader.gif" alt="loader" />
+      </div>
+      <div className="text-white-50 font-bold text-7xl leading-none absolute bottom-10 right-10">
+        {Math.floor(progress)}%
+      </div>
     </div>
   );
 };
