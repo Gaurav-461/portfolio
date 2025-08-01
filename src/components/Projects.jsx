772f9538a252c70ca850319aa48f017893084f39
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-08-01 20:17:15 +0530

    Enhance Projects component: Remove animation markers, add easing functions, and improve project card structure with custom cursor.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,6 @@ const Projects = () => {
       scrollTrigger: {
         trigger: "#projects",
         start: "top 80%",
-        // markers: true,
       },
     });
 
@@ -16,25 +15,28 @@ const Projects = () => {
       filter: "blur(5px)",
       duration: 0.5,
       opacity: 0,
+      ease: "power3.inOut",
     }).from(".project-item", {
       filter: "blur(15px)",
       yPercent: 100,
       opacity: 0,
       duration: 0.5,
       stagger: 0.2,
+      ease: "power1.in",
     });
   });
+
   return (
-    <div id="projects" className="">
+    <div id="projects">
       <h1 className="projects-title">PROJECTS</h1>
 
-      <div className="project-card">
+      <div className="project-card relative custom-cursor">
         {projectLists.map((item) => (
           <div
             key={item.id}
             className="project-item group mx-auto mt-0 border-b-2 border-white py-5 md:py-10"
           >
-            <a target="_blank" href={item.projectLink}>
+            <a target="_blank" href={item.projectLink} className="custom-cursor">
               <div className="text-white transition-all duration-300 group-hover:translate-x-4">
                 <p className="mb-2 pl-1 text-xs">{item.label.toUpperCase()}</p>
                 <h1 className="text-3xl md:text-6xl">{item.name}</h1>
