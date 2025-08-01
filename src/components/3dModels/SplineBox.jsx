106dab38f7055c6d4118f1524f094a4cd8dcf495
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-08-01 20:18:14 +0530

    Add SplineBox component: Introduce a new component to render a Spline scene from an external URL.

---
+++ src/components/3dModels/SplineBox.jsx
@@ -0,0 +1,9 @@
+import Spline from "@splinetool/react-spline";
+
+const SplineBox = () => {
+    return (
+        <Spline scene="https://prod.spline.design/ahHbM7Vca6gxi9Ue/scene.splinecode" />
+    );
+};
+
+export default SplineBox;
